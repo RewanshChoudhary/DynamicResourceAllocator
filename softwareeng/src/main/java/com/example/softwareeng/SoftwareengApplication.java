435d670746346efb77b874c54566e2dc72169dda
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2025-12-19 12:15:56 +0530

    v1

---
+++ softwareeng/src/main/java/com/example/softwareeng/SoftwareengApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwareeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareengApplication.class, args);
+	}
+
+}
